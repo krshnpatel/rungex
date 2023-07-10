fe1a0d1059d74b5637aafe9af4d909925b52a843
Krishan Patel <krishanpatel09@gmail.com>
2023-07-09 20:16:41 -0400

    Configure prettier

--- src/rungex.ts
+++ src/rungex.ts
@@ -1,21 +1,20 @@
 import { BaseCommand } from '@yarnpkg/cli';
-import { Command, Option } from 'clipanion';
+import { Command, Option, Usage } from 'clipanion';
 import t from 'typanion';
+
+const usage: Usage = {
+    description: 'Run multiple package scripts using Regex.',
+    details: `
+  This command will print a nice message.
+`,
+    examples: [['Add two numbers together', 'yarn addition 42 10']],
+};
 
 export class RungexCommand extends BaseCommand {
     static paths = [['rungex']];
 
     // Show descriptive usage for a --help argument passed to this command
-    static usage = Command.Usage({
-        description: 'Run multiple package scripts using Regex.',
-        details: `
-      This command will print a nice message.
-    `,
-        examples: [[
-            'Add two numbers together',
-            'yarn addition 42 10',
-        ]],
-    });
+    static usage = Command.Usage(usage);
 
     a = Option.String({ validator: t.isString() });
 
